121893c4a226279ec3b03b9d882158582419e59b
KádasBettina <KdasBettina@DESKTOP-K9L4O3C>
2022-04-24 16:48:59 +0200

    lol

--- sudoku/Program.cs
+++ sudoku/Program.cs
@@ -11,13 +11,16 @@ namespace sudoku
     {
         const int rowSize = 9; //konstans sorok száma
         const int columnSize = 9; //konstans oszlopok száma
+        const int depthSize = 9; //konstans z 
+        const int smallRowSize = 3;
+        const int smallColumnSize = 3;
 
-        static string[,] FileRead(string fileName) //fájlbeolvasó függvény, ami megkapja a fileName stringet a Main fgvből
+        static int[,] FileRead(string fileName) //fájlbeolvasó függvény, ami megkapja a fileName stringet a Main fgvből
         {
 
             StreamReader sr = new StreamReader(fileName); //fájlbeolvasás
 
-            string[,] table = new string[rowSize, columnSize]; //9x9 táblázat létrehozása
+            int[,] table = new int[rowSize, columnSize]; //9x9 táblázat létrehozása
 
             int rowIndex = 0;
             while (!sr.EndOfStream && rowIndex < 9) //végigmegyünk egyenként a fájl sorain
@@ -25,25 +28,71 @@ namespace sudoku
                 string[] rowFromFile = sr.ReadLine().Split(' '); //fájlsorok egyenként szétszedése oszloponként
                 for (int columnIndex = 0; columnIndex < rowFromFile.Length; columnIndex++) //számolja az oszlopokat
                 {
-                    table[rowIndex, columnIndex] = rowFromFile[columnIndex]; //egyeztetjük a fájlt a táblázattal, beleolvassuk
+                    table[rowIndex, columnIndex] = int.Parse(rowFromFile[columnIndex]); //egyeztetjük a fájlt a táblázattal, beleolvassuk
                 }
                 rowIndex++; //számozza a sorokat
             }
             return table;
         }
         
-        static void ConsoleWrite(string[,] tableFromRead) //kiíró függvény, ami megkapja a táblázatot a Main fgvből, amit a FileRead 
+        static void ConsoleWrite(int[,] tableFromRead) //kiíró függvény, ami megkapja a táblázatot a Main fgvből, amit a FileRead 
         {
             for (int rowIndex = 0; rowIndex < rowSize; rowIndex++) //végigmegy a sorokon
             {
                 for (int columnIndex = 0; columnIndex < columnSize; columnIndex++) //végigmegy az oszlopokon
                 {
-                    Console.Write(tableFromRead[rowIndex, columnIndex] + ' '); //kiír egy sort
+                    Console.Write(tableFromRead[rowIndex, columnIndex] + " "); //kiír egy sort
                 }
                 Console.WriteLine(); //sortörés, majd új sor
             }
 
         }
+
+     
+        static int[,,] ConvertToSmallTables(int[,] table) //résztáblázatokat szétszedi 3 dimenziós tömbbe
+        {
+            //colIndex/3 + (rowIndex/3 )*3 = depth
+            //smallRowIndex = rowIndex % 3
+            //smallcolIndex = colIndex % 3
+            int[,,] smallTable = new int[rowSize/3,columnSize/3,depthSize];
+
+            for(int rowIndex = 0; rowIndex < rowSize; rowIndex++)
+            {
+                for(int columnIndex = 0; columnIndex < columnSize; columnIndex++)
+                {
+                    int value = table[rowIndex, columnIndex];
+
+                    int depth = columnIndex / 3 + (rowIndex / 3) * 3;
+                    int smallRowIndex = rowIndex % 3;
+                    int smallColumnIndex = columnIndex % 3;
+
+                    smallTable[smallRowIndex, smallColumnIndex, depth] = value;
+                }
+            }
+
+         
+
+            return smallTable;
+        }
+        
+        static void ConsoleWriteSmall(int[,,] smallTable)
+        {
+
+            for (int depthIndex = 0; depthIndex < depthSize; depthIndex++)
+            {
+                for (int rowIndex = 0; rowIndex < smallRowSize; rowIndex++)
+                {
+                    for (int columnIndex = 0; columnIndex < smallColumnSize; columnIndex++)
+                    {
+                        Console.Write(smallTable[rowIndex, columnIndex, depthIndex] + " ");
+                    }
+                    Console.WriteLine("");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("\n\n");
+
+        } //résztáblák kiírása
 
         static void Main(string[] args)
         {
@@ -58,11 +107,14 @@ namespace sudoku
             int columnIndex = int.Parse(Console.ReadLine());
 
 
-            string[,] tableFromRead = Program.FileRead(fileName);
-            Program.ConsoleWrite(tableFromRead);
-            Console.WriteLine(tableFromRead[rowIndex-1,columnIndex-1]);
+            int[,] tableFromRead = Program.FileRead(fileName); //fgv meghívása a 9x9-es táblázat megírásához
 
-            
+
+            Program.ConsoleWrite(tableFromRead); //kiírja a 9x9 táblát
+            Console.WriteLine("Az adott helyen szereplő szám: " + tableFromRead[rowIndex-1,columnIndex-1]);
+
+            int[,,] smallTable = Program.ConvertToSmallTables(tableFromRead);
+            Program.ConsoleWriteSmall(smallTable);
 
             Console.ReadKey();
         }
